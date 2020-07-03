9d58f5b0b57bd4f5f2c65da4c0e2b875c7eda808
Becky <rebecabivar@hotmail.com>
2020-07-03 18:36:10 -0300

    executable done!

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# file-name-shortener
+# File Name Shortener 
+
 Literally what the name says. 
+
+## Download 
+
+> Para baixar o app, baixe o arquivo .zip e o extraia, dentro dele há uma pasta chamada "executavel". Lá está o app. 
+>
+> To download the app, download the .zip file and unzip it, inside the directory there's another one called "executavel", the app is inside it.
+
+## Como usar / How to use
+
+### PORTUGUÊS
+
+> - Copie (ou mova) o app para a pasta onde deseja cortar os nomes dos arquivos 
+>
+> - Certifique-se que não há nenhuma outra pasta dentro
+>
+>   - Se houver, ela também terá o nome cortado
+>   - Caso deseje cortar o nome da pasta também, tudo ok :P
+>
+> - É só isso, haha. :D 
+>
+>   
+>
+> **OBS.: O app apenas CORTA os nomes para diminui-lo, em breve adicionarei mais opções.**
+>
+> **Obrigada!**
+>
+> 
+
+### ENGLISH
+
+> - Move (or copy) the app to the directory which you want to cut your file names
+> - Make sure you don't have another directory inside the one you are
+>   - In case you want to make the directory name shorter, that's ok! 
+>   - If there is one, it'll have it's name shortened
+> - That's all folks! 
+>
+> **PS.: Now the app only CUTS files names to make them shorter, soon I'll be adding more options.** 
+>
+> **Thank you!** 
+
--- shortener.py
+++ shortener.py
@@ -1,16 +1,72 @@
 import glob
 import os
+from tkinter import *
 
-def rename(name_size):
+class App:
+    def __init__(self, master=None):
+        # Fonte
+        self.font = ["Yu Gothic UI Semibold", "12"]
+        # Widget 1 - Texto de boas vindas
+        self.wdg_info = Frame(master)
+        self.wdg_info.pack()
+        self.msg = Label(self.wdg_info,
+                    text = "Olá! Certifique-se de que não há outros diretórios em sua pasta.")
+        self.msg["font"] = self.font
+        self.msg.pack()
 
-    mylist = [f for f in glob.glob("*")]
+        # Widget 2 - Texto de pergunta
+        self.wdg_quest = Frame(self.wdg_info)
+        self.wdg_quest.pack()
+        self.msg = Label(self.wdg_quest,
+                    text = "Quantos caracteres você deseja que os novos nomes tenham?")
+        self.msg["font"] = self.font
+        self.msg.pack()
 
-    for file in mylist:
-        index = file.find(".")
-        extension = file[index:]
-        os.rename(file, (file[:name_size] + extension))
+        # Widget 3 - Texto de indicação da entrada do usuário
+        self.nameLabel = Label(self.wdg_quest,text="Insira aqui: ", font=self.font)
+        self.nameLabel.pack()
+
+        # Widget 4 - Caixa de input
+        self.caracters = Entry(self.wdg_quest)
+        self.caracters["width"] = 10
+        self.caracters["font"] = self.font
+        self.caracters.pack()
+
+        # Widget 5 - Botão
+        self.button = Button(self.wdg_quest)
+        self.button["text"] = "Renomear!"
+        self.button["font"] = self.font
+        self.button.pack()
+        self.button["command"] = self.rename
+
+        # Widget 6 - Mensagem de renomeado
+        self.msg_done = Label(self.wdg_quest,
+                            text = "")
+        self.msg_done["font"] = self.font
+        self.msg_done.pack()
+
+    def rename(self):
+        name_size = int(self.caracters.get())
+
+        mylist = [f for f in glob.glob("*")]
+
+        for file in mylist:
+            index = file.find(".")
+            if ((file == "shortener.py") or (file == "shortener.exe")):
+                pass
+            else:
+                extension = file[index:]
+                os.rename(file, (file[:name_size] + extension))
+
+        self.msg_done["text"] = ("Concluído! Os nomes foram encurtados para", name_size, "caracteres.")
+
+
+def app():
+    root = Tk()
+    root.title("File Name Shortener - by Bivar")
+    App(root)
+    root.mainloop()
 
 
 if __name__ == "__main__":
-    name_size = int(input("Quantos caracteres você deseja ter no nome do arquivo?"))
-    rename(name_size)
+    app()
